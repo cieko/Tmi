81ae8054428dca4be0d12f2e48905d60a095a5f0
Sciec <rahulsaxena2021@hotmail.com>
2023-06-28 18:51:35 +0530

    Environmental setup

---
+++ app/(site)/page.tsx
@@ -0,0 +1,13 @@
+
+// <!-- ========== Start Section ========== -->
+// A folder name within parenthesis are not considered as routes, so we use it to store the page (homepage)
+// file in it, to keep the folder structures clean
+// <!-- ========== End Section ========== -->
+
+export default function Home() {
+  return (
+    <div className="text-green-500">
+      Hello (Site)!
+    </div>
+  )
+}
